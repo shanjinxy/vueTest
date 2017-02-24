038573d5cf20544d1f8c3959658128e5b39ab9d3
--global <shanjin198@163.com>
2017-02-24 18:59:51 +0800

    star and bulletin is ok

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -3,11 +3,15 @@
     我是ratings
   </div>
 </template>
-<script>
-    export default{
 
-    };
-</script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
 
 </style>
+<script>
+  import axios from 'axios';
+  export default{
+    props: {
+      seller: Object
+    }
+  };
+</script>
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,12 +1,37 @@
 <template lang="html">
-  <div>
-    我是seller
+  <div class="seller-wrapper" ref="sellerWrapper">
+    <div class="seller-content">
+      <div class="info">
+        <div class="title">
+          <div class="text">{{seller.name}}</div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 <script>
-export default {
-
-};
+  import axios from 'axios';
+  export default {
+    props: {},
+    data() {
+      return {
+        seller: {},
+        collectflag: false
+      }
+    },
+    created() {
+      this._init()
+    },
+    methods: {
+      _init() {
+        axios.get('static/data.json').then((res) => {
+          this.seller = res.data.seller
+          console.log(this.seller)
+        })
+      }
+    }
+  };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 
